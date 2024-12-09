a75161f0a46f17de43be5ca339148f6c41fce23a
Sami <samiidesuu@gmail.com>
2024-12-10 00:39:16 +0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 function openProfile(url) {
   window.location.href = url;
 }
+function toggleMusic() {
+  const audio = document.getElementById("background-music");
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
